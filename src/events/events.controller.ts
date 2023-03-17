ebded139dae52d21e09c58f4f6a02f70d8532375
Vivek <vivekraj78965.vr@gmail.com>
2023-03-17 20:18:55 +0530

    a working API implemented with the help of Array

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,26 +1,47 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { CreateEventDto } from 'src/create-event.dto';
 import { UpdateEventDto } from 'src/update-event.dto';
+import { Event } from './event.entity';
 
 @Controller('events')
 export class EventsController {
+    private events:Event[] = [];
     @Get()
-    findAll(){}
+    findAll(){
+        return this.events;
+    }
 
     @Get(':id')
     findOne(@Param('id') id){
-        return id;
+        const event = this.events.find(event => event.id === +id);
+        return event;
     }
 
     @Post()
     create(@Body() input:CreateEventDto){
-        return input;
+        const event = {
+            ...input,
+            when:new Date(input.when),
+            id:this.events.length+1
+        };
+        this.events.push(event);
+        return event;
     }
 
     @Patch(":id")
-    update(@Param('id') id , @Body() input:UpdateEventDto){}
+    update(@Param('id') id , @Body() input:UpdateEventDto){
+        const index = this.events.findIndex(index => index.id === +id);
+        this.events[index] = {
+            ...this.events[index],
+            ...input,
+            when:input.when?new Date(input.when):this.events[index].when
+        };
+        return this.events[index];
+    }
 
     @Delete(':id')
     @HttpCode(204)
-    remove(@Param('id') id){}
+    remove(@Param('id') id){
+        this.events = this.events.filter(event => event.id !== +id);
+    }
 }
